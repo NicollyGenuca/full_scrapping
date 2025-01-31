c6b06fbb23d99535052a90adaa00a8a5756b5e24
Nicolly Genuca <142937580+NicollyGenuca@users.noreply.github.com>
2025-01-31 15:35:36 +0000

    adiciona scripts `lint:check e lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # full_scrapping
+
 Implemetação de projeto mapa de valor utilizando web scrapping
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home () {
-    return <h1>teste mapa de valor - job </h1>
+function Home() {
+  return <h1>teste mapa de valor - job </h1>;
 }
 
 export default Home;
